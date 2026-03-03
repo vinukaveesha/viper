e287f365d8bc43432ed4dd92b9509f70d90de871
raditha dissanayake <raditha@raditha.com>
2026-03-03 21:50:11 +0530

    Dig block display bug

--- docker/jenkins/entrypoint.sh
+++ docker/jenkins/entrypoint.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
 # Fix JENKINS_HOME ownership for rootless Podman (volume may be created with different UID).
 chown -R jenkins:jenkins /var/jenkins_home 2>/dev/null || true
-exec runuser -u jenkins -- /usr/bin/tini -s -- /usr/local/bin/jenkins.sh "$@"
+
+if [ "$(id -u)" = "0" ]; then
+  exec runuser -u jenkins -- /usr/bin/tini -s -- /usr/local/bin/jenkins.sh "$@"
+else
+  exec /usr/bin/tini -s -- /usr/local/bin/jenkins.sh "$@"
+fi
